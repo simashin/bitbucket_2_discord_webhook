fa80f8eac160c2c7a1b87d96cfae5b4366e373c3
simashin <simashin@gmail.com>
2017-04-03 16:02:37 +0300

    can translate comments

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ to start -
 2) then start server with -  morbo discord_bitbucket_webhook.pl  
 3) by default it will listen on 3000 port
 4) then you need to create Bitbucket webhook with this server url, e.g. 12.34.123.43:3000
+
+Bitbucket webhooks doc - https://confluence.atlassian.com/bitbucket/event-payloads-740262817.html
+Discord webhooks doc - https://discordapp.com/developers/docs/resources/webhook
--- discord_bitbucket_webhook.pl
+++ discord_bitbucket_webhook.pl
@@ -11,17 +11,34 @@ my $cfg = plugin 'Config' => {file => 'app.conf'};
 die 'No discord webhook url in config' unless $cfg->{discord_webhook_url};
 
 post '/' => sub {
-	my $c = shift;
-	my $bb_json = $c->req->json;
+		my $c = shift;
+		my $bb_json = $c->req->json;
 
-	my $commit_text = $bb_json->{push}{changes}[0]{commits}[0]{message};
-	my $author_name = $bb_json->{push}{changes}[0]{commits}[0]{author}{user}{display_name};
-	my $link = $bb_json->{push}{changes}[0]{commits}[0]{links}{html}{href};
-	my $msg = "new commit\nby $author_name\n$commit_text\n$link";
+		my $msg = create_message($bb_json);
+		Mojo::UserAgent->new()->post($cfg->{discord_webhook_url} => json => {content => $msg});
 
-	Mojo::UserAgent->new()->post($cfg->{discord_webhook_url} => json => {content => $msg});
-
-	$c->render(json => {res => 'Ok'});
+		$c->render(json => {res => 'Ok'});
 };
 
 app->start;
+
+
+sub create_message {
+	my $bb_json = shift;
+
+	my $msg = '';
+	if ($bb_json->{push}) {
+		my $commit_text = $bb_json->{push}{changes}[0]{commits}[0]{message};
+		my $author_name = $bb_json->{push}{changes}[0]{commits}[0]{author}{user}{display_name};
+		my $link = $bb_json->{push}{changes}[0]{commits}[0]{links}{html}{href};
+		$msg = "new commit by $author_name \" $commit_text \"\n$link\n";
+	}
+	if ($bb_json->{comment}) {
+		my $comment_text = $bb_json->{comment}{content}{raw};
+		my $author_name = $bb_json->{comment}{user}{display_name};
+		my $link = $bb_json->{comment}{links}{html}{href};
+		$msg .= "new comment by $author_name \" $comment_text \"\n$link\n";
+	}
+
+	return $msg;
+}
